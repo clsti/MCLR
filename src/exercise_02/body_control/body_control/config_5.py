correct zmp calculation

src/exercise_02/body_control/body_control/config_5.py
@@ -118,7 +118,7 @@ v_max_scaling = 0.8             # scaling velocity bounds
 x_ref = np.array([0.0, 0.0, 0.85])
 p_ref = np.array([0.0, 0.0, 0.0])
 kx_ankle = 5.0 * np.diag([1.0, 1.0, 0.0])
-kp_ankle = 2.0 * np.diag([1.0, 1.0, 0.0])
+kp_ankle = 1.0 * np.diag([1.0, 1.0, 0.0])
 
 # Hip strategy
 r_ref = np.array([0.0, 0.0, 0.0])

src/exercise_02/body_control/body_control/t51.py
@@ -465,7 +465,7 @@ class Environment(Node):
 
         # calculate zmp for double support
         self.zmp_curr_est = calc_zmp(
-            p_R, p_L, wr_ankle.linear[2], wl_ankle.linear[2])
+            w_p_R, w_p_L, wr_ankle.linear[2], wl_ankle.linear[2])
 
         # Transform ground reaction force to zmp
         w_H_zmp = pin.SE3(np.eye(3), self.zmp_curr_est)