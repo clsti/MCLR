94dcad946c2d105588b9acfcef13c190867d576b
Timo Class <timo.class@tum.de>
2025-07-22 16:11:18 +0000

    clean up dev env

--- src/project_py/walking/go2.py
+++ src/project_py/walking/go2.py
@@ -1,15 +1,10 @@
-import time
 import sys
 sys.path.insert(0, "..")  # noqa
-sys.path.insert(0, "..")  # noqa
-from crocoddyl.utils.quadruped import SimpleQuadrupedalGaitProblem
-import crocoddyl
 import pinocchio as pin
 import numpy as np
 import walking.conf_go2 as conf
 from simulator.robot import Robot
 import example_robot_data
-package_dirs = [".."]
 
 
 class Go2():
@@ -37,70 +32,6 @@ class Go2():
             useFixedBase=False
         )
 
-        self.solve_one_step()
-
-    def solve_one_step(self):
-        lfFoot, rfFoot, lhFoot, rhFoot = "FL_foot", "FR_foot", "RL_foot", "RR_foot"
-        self.problem = SimpleQuadrupedalGaitProblem(
-            self.model, lfFoot, rfFoot, lhFoot, rhFoot)
-
-        # Walking gait parameters
-        self.walking_gait = {
-            "stepLength": 0.25,
-            "stepHeight": 0.15,
-            "timeStep": 1e-2,
-            "stepKnots": 100,
-            "supportKnots": 2,
-        }
-
-        # Initialize with standing configuration
-        q0 = self.model.referenceConfigurations["standing"].copy()
-        v0 = pin.utils.zero(self.model.nv)
-
-        self.current_state = np.concatenate([q0, v0])
-
-        self.solver = crocoddyl.SolverBoxDDP(
-            self.problem.createWalkingProblem(
-                self.current_state,
-                self.walking_gait["stepLength"],
-                self.walking_gait["stepHeight"],
-                self.walking_gait["timeStep"],
-                self.walking_gait["stepKnots"],
-                self.walking_gait["supportKnots"],
-            )
-        )
-
-        # Initial guess
-        xs = [self.current_state] * (self.solver.problem.T + 1)
-        us = self.solver.problem.quasiStatic(
-            [self.current_state] * self.solver.problem.T)
-
-        # Solve
-        self.solver.solve(xs, us, 100, False, 0.1)
-
-        xs = self.solver.xs
-        xs_array = [np.array(x) for x in xs]
-
-        n_one_step = int(len(xs_array)/1)
-        xs_array_trunc = xs_array[:int(n_one_step)]
-
-        # Visualize
-        joints_to_print = ['FL_foot', 'FR_foot', 'RL_foot', 'RR_foot']
-
-        model_visu = self.model.copy()
-        data_visu = model_visu.createData()
-
-        for q in xs_array_trunc:
-            pin.forwardKinematics(model_visu, data_visu, q[:self.nq])
-            pin.updateFramePlacements(model_visu, data_visu)
-            for name in joints_to_print:
-                frame_id = self.model.getFrameId(name)
-                pos = data_visu.oMf[frame_id].translation
-                # self.sim.addSphereMarker(pos, radius=0.01)
-            self.set_position(q[:self.nq])
-            self.sim.step()
-            time.sleep(0.01)
-
     def update(self):
         self.robot.update()
 
@@ -126,19 +57,8 @@ class Go2():
 
         return np.concatenate([q, v])
 
-    def compute_inverse_dynamics(self, q_d):
-        zero_vel = np.zeros(self.nv)
-        zero_acc = np.zeros(self.nv)
-        return pin.rnea(self.model, self.go2.data, q_d, zero_vel, zero_acc)[6:]
-
     def set_torque(self, tau):
         self.robot.setActuatedJointTorques(tau)
 
     def set_position(self, pos):
         self.robot.setActuatedJointPositions(pos)
-
-    def publish(self):
-        joint_positions = self.robot.actuatedJointPosition()
-        joint_velocities = self.robot.actuatedJointVelocity()
-        print("Joint positions:", joint_positions)
-        print("Joint velocities:", joint_velocities)
--- src/project_py/walking/main.py
+++ src/project_py/walking/main.py
@@ -1,47 +1,17 @@
 import sys
 import time
 sys.path.insert(0, "..")  # noqa
-sys.path.insert(0, "..")  # noqa
 from simulator.pybullet_wrapper import PybulletWrapper
 from walking.go2 import Go2
-import pybullet as pb
-import numpy as np
-import pinocchio as pin
 
 
 def main():
     sim = PybulletWrapper()
     robot = Go2(sim)
 
-    flag = True
-
     while True:
 
-        # robot.update()
-        '''
-        if flag:
-            FL_pos = np.array(pb.getLinkState(robot.robot.id(), 3)[0])
-
-            FL_pos[2] += 0.1
-
-            flag = False
-            q = robot.robot.q()[7:]
-            v = robot.robot.v()[6:]
-
-        sim.addSphereMarker(FL_pos)
-        p = robot.robot.computeInverseKinematic(3, FL_pos)
-        p_goal = np.concatenate([q[:7], p])
-        tau = robot.compute_inverse_dynamics(p_goal)
-        # robot.set_position(p_goal)
-        robot.set_torque(tau)
-
-        data = pin.Data(robot.model)
-        tau = pin.rnea(robot.model, data, q,
-                       v, np.zeros_like(v))
-        tau_actuated = np.zeros(robot.robot.numActuatedJoints())
-        tau_actuated[robot.robot._tau_map_pin_pb] = tau
-        # robot.set_torque(tau_actuated)
-        '''
+        robot.update()
 
         # Step the simulation
         sim.step()
